cfc4acea4f05a9477301ec8f8da3581fb72b4fbc
Niebll <nabilrajankanz@gmail.comm>
2025-05-09 21:02:53 +0700

    feat: add pemesanan UI (app bar inc)

--- lib/core/nav_bar/nav_bar.dart
+++ lib/core/nav_bar/nav_bar.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:servista/core/theme/color_value.dart';
+import 'package:servista/features/booking/page/booking_page.dart';
+import 'package:servista/features/home/page/home_page.dart';
+import 'package:servista/features/profile/page/profile_page.dart';
 import 'package:servista/home_dummy.dart';
 
 class NavBar extends StatefulWidget {
@@ -14,10 +17,10 @@ class _NavBarState extends State<NavBar> {
   int _selectedIndex = 0;
 
   final List<Widget> _pages = const [
+    HomePage(),
     HomeDummy(),
-    HomeDummy(),
-    HomeDummy(),
-    HomeDummy(),
+    BookingPage(),
+    ProfilePage(),
   ];
 
   void _onItemTapped(int index) {
@@ -48,7 +51,6 @@ class _NavBarState extends State<NavBar> {
             ),
           ],
         ),
-
         child: BottomNavigationBar(
           backgroundColor: Colors.white,
           type: BottomNavigationBarType.fixed,
--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
           home: child,
         );
       },
-      child: ProfilePage(),
+      child: NavBar(),
 
     );  }
 }
